fadb5a0fd09f14826fc89f12549da330b3612f5a
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-01-27 16:35:30 +0200

    fix: karma setup

--- karma.conf.js
+++ karma.conf.js
@@ -28,14 +28,10 @@ module.exports = function(karma) {
       'test/specs/**/*.js'
     ],
 
-    // Explicitly disable hardware acceleration to make image
-    // diff more stable when ran on Travis and dev machine.
-    // https://github.com/chartjs/Chart.js/pull/5629
     customLaunchers: {
       chrome: {
         base: 'Chrome',
         flags: [
-          '--disable-accelerated-2d-canvas',
           '--disable-background-timer-throttling',
           '--disable-backgrounding-occluded-windows',
           '--disable-renderer-backgrounding'
