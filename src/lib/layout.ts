a2811fb38c2f3a0ca2f233daa5be523d5155bbf3
Corentin Pillet <corentin.pillet@energiency.com>
2025-06-30 16:25:30 +0200

    feat: Enhancing priotrity sorting

--- src/lib/layout.ts
+++ src/lib/layout.ts
@@ -265,21 +265,21 @@ export function calculateY(nodeArray: SankeyNode[], maxX: number): number {
 }
 
 export function calculateYUsingPriority(nodeArray: SankeyNode[], maxX: number) {
-  let maxY = 0
-  let nextYStart = 0
-  for (let x = 0; x <= maxX; x++) {
-    let y = nextYStart
-    const nodes = nodeArray.filter((node) => node.x === x).sort((a, b) => (a.priority ?? 0) - (b.priority ?? 0))
-    nextYStart = nodes.length
-      ? nodes[0].to.filter((to) => to.node.x > x + 1).reduce((acc, cur) => acc + cur.flow, 0) || 0
-      : 0
-    for (const node of nodes) {
-      node.y = y
-      y += Math.max(node.out, node.in)
+  let maxY = 0;
+  let nextYStart = 0;
+  for(let x = 0; x <= maxX; x++){
+    let y = nextYStart;
+    const nodes = nodeArray.filter((node)=>node.x === x).sort((a, b)=>(a.priority ?? 0) - (b.priority ?? 0));
+    console.log('nodes', nodes);
+    nextYStart = nodes.length ? nodes[0].to.filter((to)=>to.node.x > x + 1).reduce((acc, cur)=>acc + cur.flow, 0) || 0 : 0;
+    console.log('nextYStart', nextYStart);
+    for (const node of nodes){
+        node.y = y;
+        y += Math.max(node.out, node.in);
     }
-    maxY = Math.max(y, maxY)
+    maxY = Math.max(y, maxY);
   }
-  return maxY
+  return maxY;
 }
 
 type NodeXYSize = Pick<SankeyNode, 'x' | 'y' | 'size'>
