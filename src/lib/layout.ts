2f082283bfc154985211cfcbd7bf79151afb3ea0
Corentin Pillet <corentin.pillet@energiency.com>
2025-07-01 15:48:13 +0200

    Removing console.log

--- src/lib/layout.ts
+++ src/lib/layout.ts
@@ -270,9 +270,7 @@ export function calculateYUsingPriority(nodeArray: SankeyNode[], maxX: number) {
   for(let x = 0; x <= maxX; x++){
     let y = nextYStart;
     const nodes = nodeArray.filter((node)=>node.x === x).sort((a, b)=>(a.priority ?? 0) - (b.priority ?? 0));
-    console.log('nodes', nodes);
     nextYStart = nodes.length ? nodes[0].to.filter((to)=>to.node.x > x + 1).reduce((acc, cur)=>acc + cur.flow, 0) || 0 : 0;
-    console.log('nextYStart', nextYStart);
     for (const node of nodes){
         node.y = y;
         y += Math.max(node.out, node.in);
